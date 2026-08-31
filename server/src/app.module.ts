3b6cfb66ace0f880887d56b725b1ddb0eae84c86
NanaAddoDBA <nanaaddoldoe@gmail.com>
2026-08-31 05:36:53 +0200

    feat: harden api runtime operations

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,10 @@
-import { Module } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
+import { APP_INTERCEPTOR } from "@nestjs/core";
 import { ConfigModule } from "@nestjs/config";
 import { ScheduleModule } from "@nestjs/schedule";
 import { AppController } from "./app.controller";
@@ -14,12 +20,17 @@ import { IncomesModule } from "./incomes/incomes.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { SecurityModule } from "./common/security/security.module";
 import { ProfileModule } from "./profile/profile.module";
+import { validateEnvironment } from "./config/environment";
+import { RequestContextMiddleware } from "./common/request/request-context.middleware";
+import { RequestLoggingInterceptor } from "./common/request/request-logging.interceptor";
+import { OperationsModule } from "./operations/operations.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env",
+      validate: validateEnvironment,
     }),
     ScheduleModule.forRoot(),
     SecurityModule,
@@ -31,10 +42,24 @@ import { ProfileModule } from "./profile/profile.module";
     GoalsModule,
     HealthModule,
     IncomesModule,
+    OperationsModule,
     PrismaModule,
     ProfileModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RequestLoggingInterceptor,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(RequestContextMiddleware).forRoutes({
+      path: "{*path}",
+      method: RequestMethod.ALL,
+    });
+  }
+}
